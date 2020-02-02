005e1b166361e1e68ab3886f587192ae42f295ea
marckii8888 <48669622+marckii8888@users.noreply.github.com>
2020-02-02 23:48:44 +0800

    Second commit

--- src/components/Mentor/Mentor.js
+++ src/components/Mentor/Mentor.js
@@ -8,14 +8,14 @@ class Mentor extends Component {
     this.state = {};
   }
 
-  render(fullpageApi) {
+  render() {
     return (
       <div className="mentor">
         <div className="mentor-text">
           <h1>CONTACT YOUR TUTOR</h1>
           <p>
-            Do you have any questions about your course? <br/>Contact your tutor
-            and get your doubts clarified.
+            Do you have any questions about your course? <br />
+            Contact your tutor and get your doubts clarified.
           </p>
           <h2 onClick={this.props.fx}>Contact now</h2>
         </div>
